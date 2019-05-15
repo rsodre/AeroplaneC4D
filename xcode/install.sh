07361f0e6e6ac9aa48bc8ff51688906f2ee7c558
Roger Sodré <rbentos@yahoo.com>
2019-05-15 17:04:08 -0300

    Xcode updated for R20

--- xcode/install.sh
+++ xcode/install.sh
@@ -2,10 +2,10 @@
 
 # Delete C4D attributes cache
 # Find your folder in Edit > Preferences > Open Preferences folder
-export PREFERENCES_FOLDER="$HOME/Library/Preferences/MAXON/CINEMA 4D R19_8DA1506D"
+export PREFERENCES_FOLDER="$HOME/Library/Preferences/MAXON/CINEMA 4D R20_7DE41E5A"
 rm -f "$PREFERENCES_FOLDER/prefs/symbolcache"
 
 # Copy plugin to C4D
-export PLUGINS_FOLDER="/Applications/MAXON/CINEMA 4D R19/plugins"
+export PLUGINS_FOLDER="/Applications/MAXON/CINEMA 4D R20/plugins"
 rm -rf "$PLUGINS_FOLDER/Aeroplane"
 cp -R ../Aeroplane "$PLUGINS_FOLDER"
